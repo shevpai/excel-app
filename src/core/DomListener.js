3ad7b1a6e5fcbf55e97e6fa99abe4fab7bb6b0f0
shevpai <lasttrueman69@gmail.com>
2020-06-20 17:39:19 +0300

    create listeners structure

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,17 @@
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error(`No $root provided for DomeListener`)
     }
     this.$root = $root
+    this.listeners = listeners
   }
+
+  initDOMListeners() {
+    
+  }
+
+  removeDOMListeners() {
+    
+  }  
 }
--- src/core/ExelComponent.js
+++ src/core/ExelComponent.js
@@ -1,9 +1,17 @@
 import { DomListener } from "./DomListener";
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+  }
 
   // Return component tamplate
   toHTML() {
     return ''
+  }
+
+  init() {
+    this.initDOMListeners()
+
   }
 }
