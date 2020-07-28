6110a45bad650c53e95846aae49365fa952b6e0b
shevpai <lasttrueman69@gmail.com>
2020-07-28 16:32:59 +0300

    refactor toolbar tamplate

--- src/components/toolbar/toolbar.tamplate.js
+++ src/components/toolbar/toolbar.tamplate.js
@@ -13,6 +13,17 @@ function toButton(button) {
   `
 }
 
+function createOptions(state) {
+  const fontSizeArr = [6, 7, 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 32, 36]
+  
+  const options = fontSizeArr.map(size => {
+    return `      
+      <option value="${size}px" ${state.fontSize === size + 'px' ? 'selected="selected"' : ''}>${size}</option>
+    `    
+  })
+
+  return options.join('')
+}
 
 function createToolbarTextAlign(state) {
   const buttons = [
@@ -87,30 +98,13 @@ function createColorPic(state) {
 }
 
 function createFontSizeChanger(state) {
-  console.log(state)
   return `
-    <div class="font-size">
-      <select data-type="font-size">
-        <option value="6px" ${state.fontSize === '6px' ? 'selected="selected"' : ''}>6</option>
-        <option value="7px" ${state.fontSize === '7px' ? 'selected="selected"' : ''}>7</option>
-        <option value="8px" ${state.fontSize === '8px' ? 'selected="selected"' : ''}>8</option>
-        <option value="9px" ${state.fontSize === '9px' ? 'selected="selected"' : ''}>9</option>
-        <option value="10px" ${state.fontSize === '10px' ? 'selected="selected"' : ''}>10</option>
-        <option value="11px" ${state.fontSize === '11px' ? 'selected="selected"' : ''}>11</option>
-        <option value="12px" ${state.fontSize === '12px' ? 'selected="selected"' : ''}>12</option>
-        <option value="14px" ${state.fontSize === '14px' ? 'selected="selected"' : ''}>14</option>
-        <option value="16px" ${state.fontSize === '16px' ? 'selected="selected"' : ''}>16</option>
-        <option value="18px" ${state.fontSize === '18px' ? 'selected="selected"' : ''}>18</option>
-        <option value="20px" ${state.fontSize === '20px' ? 'selected="selected"' : ''}>20</option>
-        <option value="22px" ${state.fontSize === '22px' ? 'selected="selected"' : ''}>22</option>
-        <option value="24px" ${state.fontSize === '24px' ? 'selected="selected"' : ''}>24</option>
-        <option value="26px" ${state.fontSize === '26px' ? 'selected="selected"' : ''}>26</option>
-        <option value="28px" ${state.fontSize === '28px' ? 'selected="selected"' : ''}>28</option>
-        <option value="32px" ${state.fontSize === '32px' ? 'selected="selected"' : ''}>32</option>
-        <option value="36px" ${state.fontSize === '36px' ? 'selected="selected"' : ''}>36</option>
-      </select>
-    </div>
-  `
+      <div class="font-size">
+        <select data-type="font-size">
+          ${createOptions(state)}
+        </select>
+      </div>  
+    `  
 }
 
 
